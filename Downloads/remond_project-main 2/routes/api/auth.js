8a39d10fafbc3127dfec3c91a7ea73415e3a6657
ibrahim115 <49085486+ibrahim115@users.noreply.github.com>
2021-11-28 19:59:18 +0700

    Commit video 11 new

---
+++ Downloads/remond_project-main 2/routes/api/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// @routeGET api/auth
+// @descTest route
+// @accessPublic atau Private
+
+router.get('/', (req, res) => res.send('Auth route'));
+module.exports = router;
---
+++ Downloads/remond_project-main 2/routes/api/profile.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// @routeGET api/profile
+// @descTest route
+// @accessPublic atau Private
+
+router.get('/', (req, res) => res.send('Profile route'));
+module.exports = router;
